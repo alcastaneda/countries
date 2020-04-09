8287fffbb2d182721c35885884da63cb0adf6a03
Adriana <adriana.lcs316@gmail.com>
2020-04-08 21:15:22 -0400

    code cleanup: delete uneeded files

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,11 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
 import CountryData from "./pages/countryData";
 
-// import injectStyles from "./styles";
-
-// previous variable declarations
-
-// injectStyles();
 ReactDOM.render(
   <BrowserRouter>
     <Route exact path="/" component={CountryData} />
